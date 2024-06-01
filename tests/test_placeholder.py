51ae452bc048bad9143885feff7e4e55ec4ce6f7
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2024-06-01 16:13:09 +0300

    [fix]  change code for pylint rules

--- tests/test_placeholder.py
+++ tests/test_placeholder.py
@@ -4,6 +4,6 @@ Test placeholder for the project to build
 
 def test_placeholder():
     """
-    Empty test that allows test and coverage workflows to pass. In future, the most of the package should be covered with tests
+    Empty test that allows test and coverage workflows to pass. 
+    In future, the most of the package should be covered with tests
     """
-    pass
