477732f44b551d624dd6df65770cb3339611d429
Ilia Afanasev <szrnamerg@gmail.com>
2024-10-20 22:03:34 +0200

    [fix] resolve the outgroup detection issue (#31)
    using regexp for non-terminal clades detection

--- src/corpus_distance/clusterisation/utils.py
+++ src/corpus_distance/clusterisation/utils.py
@@ -4,6 +4,7 @@ by preparing existing data for the clustering functions.
 """
 
 from os.path import dirname, isdir, join, realpath
+import re
 import matplotlib
 import matplotlib.pyplot as plt
 from Bio import Phylo
@@ -56,8 +57,8 @@ def detect_outgroup(tree: Phylo.BaseTree.Tree, outgroup: str, data_name: str,
     is_outgroup_correct = 'CORRECT'\
         if outgroup in [tree.clade.clades[0].name, tree.clade.clades[1].name]\
         else 'INCORRECT'
-    outgroup_clade = 0 if tree.clade.clades[0].is_terminal() else 1
-    ingroup_clade = 1 if tree.clade.clades[0].is_terminal() else 0
+    outgroup_clade = 1 if re.search(r'Inner\d{1,}', tree.clade.clades[0].name) else 0
+    ingroup_clade = 0 if re.search(r'Inner\d{1,}', tree.clade.clades[0].name) else 1
     with open(join(store_path, metrics + ".info"), 'w', encoding='utf-8') as out:
         out.write(f"{data_name}\t{is_outgroup_correct}\t\
                   {tree.clade.clades[outgroup_clade].branch_length}\t\
