dc69bf67146bcb4247053079f23a81b94fcaa738
The-One-Who-Speaks-and-Depicts <szrnamerg@gmail.com>
2026-08-06 21:08:31 +0200

    AMEND: add logs/tests/description for plot_shingle_diversity_barchart

--- src/corpus_distance/visualisation/shingle_processing.py
+++ src/corpus_distance/visualisation/shingle_processing.py
@@ -1,10 +1,14 @@
 """
-Visualises the results of shingle processing
+Visualises the results of shingle processing. Contains the following functions:
+
+* plot_shingle_diversity_barchart: produces a stacked barchart that shows, how much
+unique n-shingles there are in the texts of the dataset for each n in the given range.
 """
 from logging import getLogger, NullHandler
 
-from numpy import zeros
-from matplotlib.pyplot import subplots, close
+from numpy import zeros, issubdtype, number
+from matplotlib.pyplot import subplots, close, Figure
+from collections.abc import Iterable
 
 logger = getLogger(__name__)
 logger.addHandler(NullHandler())
@@ -14,12 +18,50 @@ def plot_shingle_diversity_barchart(
         bottom_limit: int = 1,
         upper_limit: int = 16,
         width: float = 0.5
-        ):
+        ) -> Figure:
     """
-    Takes 
+    Takes a dictionary with lect names as keys and quantities of n-shingles
+    (with n from bottom_limit to upper_limit) in the texts of these lect, and builds
+    a stacked bar chart with the width being set by the width parameter.
+
+    Parameters:
+        quantities_dict (dict): a dictionary with strings as keys (names of entities,
+        in this case, lects), and lists of integers as values.
+        bottom_limit (int): the value that denotes the first n, by which the dataset tokens
+        were split.
+        uppper_limit (int): the value that denotes the value next to the last n,
+        by which the dataset tokens were split.
+        width (float): the width of the bar.
+    
+    Returns:
+        Figure: a bar chart that on its X-axis has different n-s,
+        into the sequences of length of which the texts got split, on its Y-axis has the overall
+        number of n-shingles for a specific n, the colors denoting the lects (or any entities).
     """
     logger.debug("Input params: %s", locals())  
-
+    if not isinstance(quantities_dict, dict) or len(quantities_dict.keys()) == 0 or not all(
+        isinstance(key, str) for key in quantities_dict.keys()
+    ) or not all(
+        isinstance(val, Iterable) and all(
+            issubdtype(type(num), number) for num in val
+        ) and len(val) == len(
+            list(quantities_dict.values())[0]
+            ) for val in quantities_dict.values()
+    ):
+        raise ValueError("quantities_dict should be a dict")
+    if not isinstance(bottom_limit, int) or bottom_limit < 1:
+        raise ValueError(f"bottom_limit should be a positive integer, received {bottom_limit}")
+    if not isinstance(upper_limit, int) or upper_limit < 1 or upper_limit < bottom_limit:
+        raise ValueError("upper_limit should be a positive integer"
+                         f"more than bottom_limit({bottom_limit}), received {upper_limit}")
+    if not issubdtype(type(width), number):
+        raise ValueError(f"width should be a number, received {width}")
+    if width <= 0:
+        # this is not a problem for building a bar chart, the bars are just going to be
+        # one pixel in length; still, I think it is necessary to give a warning to a user
+        logger.warning(
+            "Width (%s) is negative or neutral, reconsider setting the parameter", width
+            )
     fig, ax = subplots()
     range_of_ns = range(bottom_limit, upper_limit)
     current_layer_bottom = zeros(upper_limit - bottom_limit)
--- tests/cases/visualisation/test_shingle_processing.py
+++ tests/cases/visualisation/test_shingle_processing.py
@@ -1,4 +1,6 @@
-
+"""
+Contains tests for the functions of the corpus_distance.visualistion.shingle_processing module. 
+"""
 from numpy import array
 import matplotlib
 import matplotlib.pyplot as plt
@@ -7,7 +9,7 @@ import unittest
 
 
 from corpus_distance.visualisation.shingle_processing import (
-    plot_shingle_diversity_barchart
+    logger, plot_shingle_diversity_barchart
 )
 
 from tests.mocks.data_preprocessing import SHINGLE_FREQUENCIES
@@ -15,12 +17,176 @@ from tests.mocks.data_preprocessing import SHINGLE_FREQUENCIES
 matplotlib.use('Agg')
 
 class TestPlotShingleDiversityBarchart(unittest.TestCase):
+    """
+    Contains tests for the plot_shingle_diversity_barchart function.
+    """
 
     def tearDown(self):
+        """
+        Closes all possible figures.
+        """
         plt.close('all')
 
+    def test_plot_shingle_diversity_barchart_bad_cases(self):
+        """
+        Checks, whether the function correctly processes incorrect arguments.
+        """
 
-    def test_correct_case(self):
+        for case in [
+            {
+                "shingle_frequencies": {},
+                "bottom": 1,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": {'lect1': None},
+                "bottom": 1,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": {'lect1': ['1', 1, 0.5]},
+                "bottom": 1,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": {'lect1': [1, 1, 1], 'lect2': [1, 2]},
+                "bottom": 1,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": [],
+                "bottom": 1,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": 0,
+                "bottom": 1,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": 'gf',
+                "bottom": 1,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 0,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 0.5,
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": '1',
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": [1],
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": {1: 2},
+                "upper": 16,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 1,
+                "upper": -1,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 0,
+                "upper": '16',
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 0,
+                "upper": False,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 0,
+                "upper": {16: 32},
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 0,
+                "upper": [16],
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 3,
+                "upper": 2,
+                "width": 0.5
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 1,
+                "upper": 5,
+                "width": '0.5'
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 1,
+                "upper": 5,
+                "width": [0.5]
+            },
+            {
+                "shingle_frequencies": SHINGLE_FREQUENCIES,
+                "bottom": 1,
+                "upper": 5,
+                "width": {0.5: 0.5}
+            },
+        ]:
+            with self.subTest(data=case):
+                self.assertRaises(
+                    ValueError,
+                    plot_shingle_diversity_barchart,
+                    case["shingle_frequencies"],
+                    case["bottom"],
+                    case["upper"],
+                    case["width"]
+                )
+
+    def test_plot_shingle_diversity_barchart_bad_cases_non_positive_width(self):
+        """
+        Checks, whether the function warns in case the width parameter is 0 or less.
+        """
+        warning = "WARNING:corpus_distance.visualisation.shingle_processing:" \
+        "Width (0) is negative or neutral, reconsider setting the parameter"
+
+        with self.assertLogs(logger.name, "WARNING") as captured:
+            plot_shingle_diversity_barchart(SHINGLE_FREQUENCIES, 1, 16, 0)
+
+        self.assertTrue(warning in captured.output)
+
+
+    def test_plot_shingle_diversity_barchart_correct_case(self):
+        """
+        Checks, whether the function correctly processes the default correct parameters.
+        """
         expected = [
             n_shingle_freq for lect in SHINGLE_FREQUENCIES.values() for n_shingle_freq in array(
                 lect, dtype=float
